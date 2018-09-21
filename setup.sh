cf8a3ad3e3be4a63db463ff11c5b2dbbd6b361ef
Eddie Wang <eddie@vest.com>
2018-09-21 12:14:36 -0400

    .

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-if [ ! -f ~/.vest_haskell ]; then
-  stack build --copy-compiler-tool intero phoityne-vscode hindent hlint
-  brew install rabbitmq libpq postgres
-  touch ~/.vest_haskell
-fi
+stack build --copy-compiler-tool intero phoityne-vscode hindent hlint
+brew install rabbitmq libpq postgres
