3a802d9a949c5810fbc2ef87543dcd122b1dd2b3
Eddie Wang <eddie@vest.com>
2018-09-21 12:51:39 -0400

    setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-stack build --copy-compiler-tool intero phoityne-vscode hindent hlint
+stack build --copy-compiler-tool intero phoityne-vscode hlint
+stack install hindent
 brew install rabbitmq libpq postgres
