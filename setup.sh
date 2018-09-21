a6bfd9305d3e7a87f15e307ce9121b28abac3a7e
Sanjay Kannan <whaatt@gmail.com>
2018-09-21 12:09:22 -0400

    Extensions update.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-stack build --copy-compiler-tool intero phoityne-vscode hindent hlint
-brew install rabbitmq libpq postgres
+if [ ! -f ~/.vest_haskell ]; then
+  stack build --copy-compiler-tool intero phoityne-vscode hindent hlint
+  brew install rabbitmq libpq postgres
+  touch ~/.vest_haskell
+fi
