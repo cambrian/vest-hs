38cfc8591e6a0d32ed2a9b05c29252d85dd2def6
Sanjay Kannan <whaatt@gmail.com>
2018-09-28 13:59:14 -0400

    bump types

--- bridge-client/bin/Types.hs
+++ bridge-client/bin/Types.hs
@@ -3,10 +3,30 @@ import Bridge.Transports.WebSocket (RequestMessage, ResponseMessage)
 import BridgeClient
 import Data.Aeson.TypeScript.TH
 import Data.Aeson.Types ()
+import Data.Text (unlines)
 import qualified Manager
 import System.Directory
 import System.FilePath
+import Text.Replace
 import VestPrelude
+
+nominal :: Text
+nominal =
+  unlines
+    [ "class Nominal<T extends string> {"
+    , "  // @ts-ignore"
+    , "  private as: T"
+    , "}\n"
+    ]
+
+replaceRules :: [Replace]
+replaceRules =
+  [ Replace
+      "type Text_<T> = IText_<T>"
+      "type Text_<T extends string> = IText_<T> & Nominal<T>"
+  , Replace "\"" "\'"
+  , Replace ";" ""
+  ]
 
 main :: IO ()
 main = do
@@ -16,16 +36,19 @@ main = do
     case head arguments of
       Just _file -> return _file
       Nothing -> die "no output file provided"
-  writeFile
-    (wd </> file)
-    (pack . formatTSDeclarations . concat $
-     [ getTypeScriptDeclarations (Proxy :: Proxy SerializationFormat)
-     , getTypeScriptDeclarations (Proxy :: Proxy Headers)
-     , getTypeScriptDeclarations (Proxy :: Proxy Text')
-     , getTypeScriptDeclarations (Proxy :: Proxy RpcClientException)
-     , getTypeScriptDeclarations
-         (Proxy :: Proxy (Either RpcClientException Text))
-     , getTypeScriptDeclarations (Proxy :: Proxy RequestMessage)
-     , getTypeScriptDeclarations (Proxy :: Proxy ResponseMessage)
-     , generateTsDeclarations (Proxy :: Proxy Manager.ManagerApi)
-     ])
+  writeFile (wd </> file) $
+    pack . replaceWithList replaceRules $
+    unpack
+      (nominal <>
+       (pack . formatTSDeclarations . concat $
+        [ getTypeScriptDeclarations (Proxy :: Proxy SerializationFormat)
+        , getTypeScriptDeclarations (Proxy :: Proxy Headers)
+        , getTypeScriptDeclarations (Proxy :: Proxy Text')
+        , getTypeScriptDeclarations (Proxy :: Proxy RpcClientException)
+        , getTypeScriptDeclarations
+            (Proxy :: Proxy (Either RpcClientException Text))
+        , getTypeScriptDeclarations (Proxy :: Proxy RequestMessage)
+        , getTypeScriptDeclarations (Proxy :: Proxy ResponseMessage)
+        , generateTsDeclarations (Proxy :: Proxy Manager.ManagerApi)
+        ]) <>
+       "\n")
