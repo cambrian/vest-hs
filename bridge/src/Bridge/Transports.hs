a2686525a37504495c22f271eb3e4323a46c0f76
Eddie Wang <eddie@vest.com>
2018-10-01 14:20:29 -0400

    .

--- bridge/src/Bridge/Transports.hs
+++
@@ -1,7 +0,0 @@
-module Bridge.Transports
-  ( module Reexports
-  ) where
-
-import Bridge.Transports.Amqp as Reexports
--- import Bridge.Transports.Dummy as Reexports
--- import Bridge.Transports.WebSocket as Reexports
