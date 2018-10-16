c58d77cf7c3af5c0b164b0f9a90d524246d3fb42
Sanjay Kannan <whaatt@gmail.com>
2018-10-16 17:10:32 -0400

    add a tzscan endpoint

--- tezos/bin/Example.hs
+++ tezos/bin/Example.hs
@@ -1,5 +1,6 @@
 import qualified Stream
 import Tezos.Node
+import Tezos.TzScan
 import Vest
 import Vest.Http
 
@@ -7,29 +8,34 @@ publicTezosConfig :: Config
 publicTezosConfig =
   Config {schemeType = HttpsType, host = "rpc.tezrpc.me", port = 443, path = ""}
 
+publicTzScanConfig :: Config
+publicTzScanConfig =
+  Config
+    {schemeType = HttpsType, host = "api5.tzscan.io", port = 443, path = ""}
+
+printRewardSplit :: T -> Text -> Int -> Int -> IO ()
+printRewardSplit connection address cycle page =
+  direct
+    (request (Proxy :: Proxy GetRewardsSplit) address cycle page)
+    connection >>=
+  print
+
 printMainHeadContractsCount :: T -> IO ()
-printMainHeadContractsCount connection = do
-  contracts <-
-    direct
-      (request (Proxy :: Proxy ListContracts) mainChain headBlock)
-      connection
-  print $ length contracts
+printMainHeadContractsCount connection =
+  direct (request (Proxy :: Proxy ListContracts) mainChain headBlock) connection >>=
+  (print . length)
 
 printContractManager :: T -> Text -> IO ()
-printContractManager connection contract = do
-  manager <-
-    direct
-      (request (Proxy :: Proxy GetContractManager) mainChain headBlock contract)
-      connection
-  print manager
+printContractManager connection contract =
+  direct
+    (request (Proxy :: Proxy GetContractManager) mainChain headBlock contract)
+    connection >>=
+  print
 
 printMainHeadConstants :: T -> IO ()
-printMainHeadConstants connection = do
-  constants <-
-    direct
-      (request (Proxy :: Proxy GetConstants) mainChain headBlock)
-      connection
-  print constants
+printMainHeadConstants connection =
+  direct (request (Proxy :: Proxy GetConstants) mainChain headBlock) connection >>=
+  print
 
 printBlocks :: T -> IO ()
 printBlocks connection = do
@@ -39,6 +45,8 @@ printBlocks connection = do
 main :: IO ()
 main = do
   connection <- make publicTezosConfig
+  connectionTzScan <- make publicTzScanConfig
+  printRewardSplit connectionTzScan "tz1Zhv3RkfU2pHrmaiDyxp7kFZpZrUCu1CiF" 36 0
   printMainHeadContractsCount connection
   printContractManager connection "KT1Xnjog1ou1HNHQNsD9nVi3ddkb9YQ5f28k"
   printMainHeadConstants connection
