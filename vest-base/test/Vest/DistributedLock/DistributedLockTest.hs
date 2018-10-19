e805f9a6c7188307cfbeff06cef6c2b59ced101b
Eddie Wang <eddie@vest.com>
2018-10-19 12:52:52 -0400

    not fixed but tmvar -> tvar is an improvement

--- vest-base/test/Vest/DistributedLock/DistributedLockTest.hs
+++ vest-base/test/Vest/DistributedLock/DistributedLockTest.hs
@@ -15,42 +15,34 @@ simpleConcurrentTest =
   with
     localRedisConfig
     (\connection -> do
-       let key = "simpleConcurrent"
-       result <- newTMVarIO ""
+       let lockId = "simpleConcurrent"
+       result <- newTVarIO ""
        -- Wait 30 ms then try to acquire lock.
        -- Note that even though this thread starts after threadC, it gets the lock first because
        -- threadC is stuck in a polling sleep. (Unfortunate, but what to do with polling?)
        threadA <-
          async $ do
            threadDelay (ms 30)
-           with @DistributedLock (testLock connection key) $
-             const $
-             atomically $ do
-               current <- readTMVar result
-               swapTMVar result (current <> "A")
+           with @DistributedLock (testLock connection lockId) $
+             const $ atomically $ modifyTVar result (<> "A")
       -- Acquire lock then sleep for 20 ms.
       -- Test renewals by changing this to 3 seconds.
        threadB <-
          async $
-         with @DistributedLock (testLock connection key) $
+         with @DistributedLock (testLock connection lockId) $
          const $ do
            threadDelay (ms 20)
-           atomically $ do
-             current <- readTMVar result
-             swapTMVar result (current <> "B")
+           atomically $ modifyTVar result (<> "B")
        -- Wait 10 ms then try to acquire lock.
        threadC <-
          async $ do
            threadDelay (ms 10)
-           with @DistributedLock (testLock connection key) $
-             const $
-             atomically $ do
-               current <- readTMVar result
-               swapTMVar result (current <> "C")
+           with @DistributedLock (testLock connection lockId) $
+             const $ atomically $ modifyTVar result (<> "C")
        wait threadA
        wait threadB
        wait threadC
-       atomically $ takeTMVar result)
+       readTVarIO result)
 
 test_distributed_lock :: TestTree
 test_distributed_lock = testGroup "DistributedLock" [simpleConcurrentTest]
