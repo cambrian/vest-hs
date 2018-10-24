4a1182c686e0242276decd985ef8d169b9e5665c
Sanjay Kannan <whaatt@gmail.com>
2018-10-24 14:47:08 +0200

    Change organization of DummyManager.

--- services/dummy-manager/src/DummyManager.hs
+++ services/dummy-manager/src/DummyManager.hs
@@ -4,77 +4,16 @@ module DummyManager
   ( module DummyManager
   ) where
 
-import Data.Aeson.TypeScript.TH
-import Data.Aeson.Types
+import DummyManager.Api as DummyManager
+import qualified DummyManager.Auth as DummyAuth
+import DummyManager.Internal as DummyManager
+import qualified Stream
 import qualified Transport.WebSocket as WebSocket
 import Vest
-
-data DummyAuth =
-  DummyAuth
-
-instance RequestVerifier DummyAuth where
-  type VerifierClaims DummyAuth = ()
-  verifyRequest _ _ _ = return $ Right ()
-
-instance RequestSigner DummyAuth where
-  signRequest _ headers _ = return headers
-
-instance Auth DummyAuth where
-  type AuthVerifier DummyAuth = DummyAuth
-  type AuthSigner DummyAuth = DummyAuth
 
 data DummyManager = Args
   {
   } deriving (Eq, Show, Read, Generic, Data)
-
-data T = T
-  { dummyAuth :: DummyAuth
-  , webSocket :: WebSocket.T
-  }
-
-instance HasAuthVerifier DummyAuth T where
-  authVerifier = dummyAuth
-
-instance HasRpcTransport WebSocket.T T where
-  rpcTransport = webSocket
-
-data AddIntsRequest = AddIntsRequest
-  { a :: Int
-  , b :: Int
-  } deriving (Eq, Ord, Show, Read, Generic, Hashable, ToJSON, FromJSON)
-
-$(deriveTypeScript defaultOptions ''AddIntsRequest)
-
-type AddIntsEndpoint
-   = Endpoint_ 5 'JSON 'NoAuth T WebSocket.T "addInts" AddIntsRequest ('Direct Int)
-
-type AddIntsBadEndpoint
-   = Endpoint_ 0 'JSON 'NoAuth T WebSocket.T "addIntsBad" AddIntsRequest ('Direct Int)
-
-type EchoThriceEndpoint
-   = Endpoint_ 5 'JSON 'NoAuth T WebSocket.T "echoThrice" Int ('Streaming Int)
-
-type EchoThriceBadEndpoint
-   = Endpoint_ 0 'JSON 'NoAuth T WebSocket.T "echoThriceBad" Int ('Streaming Int)
-
-data ConcatTextAuthRequest = ConcatTextAuthRequest
-  { a :: Text
-  , b :: Text
-  } deriving (Eq, Ord, Show, Read, Generic, Hashable, ToJSON, FromJSON)
-
-$(deriveTypeScript defaultOptions ''ConcatTextAuthRequest)
-
-data ConcatTextAuthResponse = ConcatTextAuthResponse
-  { result :: Text
-  } deriving (Eq, Ord, Show, Read, Generic, Hashable, ToJSON, FromJSON)
-
-$(deriveTypeScript defaultOptions ''ConcatTextAuthResponse)
-
-type ConcatTextAuthEndpoint
-   = Endpoint_ 5 'JSON ('Auth DummyAuth) T WebSocket.T "concatTextAuth" ConcatTextAuthRequest ('Direct ConcatTextAuthResponse)
-
-type EchoThriceAuthEndpoint
-   = Endpoint_ 5 'JSON ('Auth DummyAuth) T WebSocket.T "echoThriceAuth" Text ('Streaming Text)
 
 type Api
    = AddIntsEndpoint
@@ -84,12 +23,35 @@ type Api
      :<|> ConcatTextAuthEndpoint
      :<|> EchoThriceAuthEndpoint
 
+addInts :: T -> AddIntsRequest -> IO Int
+addInts _ AddIntsRequest {a, b} = do
+  threadDelay (sec 0.25)
+  return (a + b)
+
+echoThrice :: T -> Int -> IO (Stream Int)
+echoThrice _ x = do
+  threadDelay (sec 0.25)
+  return . Stream.fromList . replicate 3 $ x
+
+concatTextAuth ::
+     T
+  -> VerifierClaims DummyAuth.T
+  -> ConcatTextAuthRequest
+  -> IO ConcatTextAuthResponse
+concatTextAuth _ _ ConcatTextAuthRequest {a, b} =
+  return $ ConcatTextAuthResponse {result = a <> b}
+
+echoThriceAuth :: T -> VerifierClaims DummyAuth.T -> Text -> IO (Stream Text)
+echoThriceAuth _ _ = return . Stream.fromList . replicate 3
+
+handlers :: Handlers Api
+handlers =
+  addInts :<|> addInts :<|> echoThrice :<|> echoThrice :<|> concatTextAuth :<|>
+  echoThriceAuth
+
 instance Service T where
   type ServiceArgs T = DummyManager
   type PublishSpec T = ()
   type RpcSpec T = Api
   defaultArgs = Args {}
-  init _ f =
-    with
-      WebSocket.localConfig
-      (\webSocket -> f $ T {dummyAuth = DummyAuth, webSocket})
+  init _ f = with WebSocket.localConfig (\webSocket -> f $ T {webSocket})
