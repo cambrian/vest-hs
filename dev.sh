df247a6c032fc09104e040212129e761c91d8616
Sanjay Kannan <whaatt@gmail.com>
2018-11-03 01:57:37 +0100

    Bump client.

--- dev.sh
+++ dev.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-set -e
-set -o pipefail
-
 rm -f rabbit.log
 /usr/local/bin/redis-cli shutdown > /dev/null 2>&1
 /usr/local/bin/redis-server /usr/local/etc/redis.conf > ~/.vest-hs/redis-run.log &
--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-set -e
-set -o pipefail
-
 CYAN='\033[0;36m'
 NC='\033[0m'
 
