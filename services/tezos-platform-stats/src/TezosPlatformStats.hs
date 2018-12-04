f6eabaf49a4889b48fc0b19f31c71fe902f0ece6
Eddie Wang <eddie@vest.com>
2018-12-03 17:49:10 -0800

    .

--- services/tezos-platform-stats/src/TezosPlatformStats.hs
+++ services/tezos-platform-stats/src/TezosPlatformStats.hs
@@ -3,82 +3,41 @@ module TezosPlatformStats
   ( module TezosPlatformStats
   ) where
 
-import qualified Data.HashMap.Strict as HashMap
+import qualified Postgres as Pg
 import qualified Tezos
+import qualified TezosChainWatcher.Db
 import TezosPlatformStats.Api as TezosPlatformStats
 import TezosPlatformStats.Internal as TezosPlatformStats
 import Vest
 
-overview :: T -> () -> IO TezosPlatformStats.OverviewResponse
-overview T {rawStubData} _ = do
-  stubData <- deserializeUnsafe @'JSON rawStubData
-  return $ overviewResponse stubData
-
-bakersFn :: T -> () -> IO TezosPlatformStats.BakersResponse
-bakersFn T {rawStubData} _ = do
-  stubData <- deserializeUnsafe @'JSON rawStubData
-  return $ bakersResponse stubData
-
-implicit :: T -> Tezos.ImplicitAddress -> IO TezosPlatformStats.ImplicitResponse
-implicit T {rawStubData} implicitPkh = do
-  stubData <- deserializeUnsafe @'JSON rawStubData
-  case HashMap.lookup implicitPkh (implicitResponse stubData) of
-    Nothing -> throw $ InvalidCallException "no such implicit PKH"
-    Just response -> return response
-
-operation ::
-     T
-  -> Tezos.OperationHash
-  -> IO (Stream ValueBuffer TezosPlatformStats.OperationResponse)
-operation T {rawStubData, streamDelayMillis} opHash = do
-  stubData <- deserializeUnsafe @'JSON rawStubData
-  case HashMap.lookup opHash (operationResponses stubData) of
-    Nothing -> throw $ InvalidCallException "no such operation hash"
-    Just streamOpList -> do
-      (writer, stream) <- newStream
-      async $
-        mapM_
-          (\x ->
-             threadDelay (ms $ toRational streamDelayMillis) >>
-             writeStream writer x)
-          streamOpList >>
-        closeStream writer
-      return stream
-
-newtype RawStubData =
-  RawStubData Text
-  deriving newtype (IsString, FromJSON)
-
-instance Loadable RawStubData where
-  configFile = [relfile|stub-data.json|]
+originationsByImplicitAddress ::
+     T -> Tezos.ImplicitAddress -> IO [Tezos.OriginatedAddress]
+originationsByImplicitAddress T {chainWatcherDb} addr = do
+  Pg.runLogged chainWatcherDb $
+    Pg.runSelectReturningList $
+    Pg.select $
+    TezosChainWatcher.Db.originated <$>
+    Pg.filter_
+      (\TezosChainWatcher.Db.Origination {originator} ->
+         originator Pg.==. Pg.val_ addr)
+      (Pg.all_ $ (TezosChainWatcher.Db.originations TezosChainWatcher.Db.schema))
+    -- SELECT originated from originations where originator == addr
 
 instance Service T where
-  type RpcSpec T = OverviewEndpoint
-                   :<|> BakersEndpoint
-                   :<|> ImplicitEndpoint
-                   :<|> OperationEndpoint
+  type RpcSpec T = OriginationsByImplicitAddressEndpoint
   type ValueSpec T = ()
   type EventsProduced T = ()
   type EventsConsumed T = ()
   summary = "tezos-platform-stats v0.1.0"
   description = "Front-end stats server for Tezos."
   init configPaths f = do
-    (RawStubData rawStubData) <- load configPaths
-    withLoadable configPaths $ \(webSocket :<|> redis) ->
-      f $ T {webSocket, redis, rawStubData, streamDelayMillis = 500}
-  rpcHandlers t = overview t :<|> bakersFn t :<|> implicit t :<|> operation t
+    withLoadable configPaths $ \(webSocket :<|> amqp :<|> coreDb :<|> chainWatcherDb :<|> redis) ->
+      f $ T {webSocket, amqp, coreDb, chainWatcherDb, redis}
+  rpcHandlers t = originationsByImplicitAddress t
   valuesPublished _ = ()
   eventProducers _ = ()
   eventConsumers _ = ()
 
-type PublicApi = RpcSpec T
+type PublicApi = ()
 
-type AuxiliaryTypes
-   = Baker
-     :<|> DelegateFraction
-     :<|> DelegateInfo
-     :<|> LedgerOperation
-     :<|> LedgerOperationType
-     :<|> OriginatedAddress
-     :<|> TimeRate
-     :<|> TimeSize
+type AuxiliaryTypes = ()
--- services/tezos-platform-stats/src/TezosPlatformStats/Internal.hs
+++ services/tezos-platform-stats/src/TezosPlatformStats/Internal.hs
@@ -2,14 +2,19 @@ module TezosPlatformStats.Internal
   ( module TezosPlatformStats.Internal
   ) where
 
+import qualified Amqp
+import qualified Postgres
+import qualified TezosChainWatcher.Internal as TezosChainWatcher
+import qualified TezosPlatformCore.Internal as TezosPlatformCore
 import Vest
 import qualified WebSocket
 
 data T = T
   { webSocket :: WebSocket.T
+  , amqp :: Amqp.T
+  , coreDb :: Pool (Specific TezosPlatformCore.T Postgres.Connection)
+  , chainWatcherDb :: Pool (Specific TezosChainWatcher.T Postgres.Connection)
   , redis :: RedisConnection
-  , rawStubData :: Text
-  , streamDelayMillis :: Natural
   }
 
 instance HasNamespace T where
@@ -18,5 +23,14 @@ instance HasNamespace T where
 instance Has WebSocket.T T where
   get = webSocket
 
+instance Has Amqp.T T where
+  get = amqp
+
 instance Has RedisConnection T where
   get = redis
+
+instance Has (Pool (Specific TezosPlatformCore.T Postgres.Connection)) T where
+  get = coreDb
+
+instance Has (Pool (Specific TezosChainWatcher.T Postgres.Connection)) T where
+  get = chainWatcherDb
