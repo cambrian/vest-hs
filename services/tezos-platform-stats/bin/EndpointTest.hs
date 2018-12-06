9936041d40dab1bcb2b1d174d5aea7a9c20b2f21
z1awrence <zack@vest.com>
2018-12-06 14:09:03 -0800

    simple endpoint test added

---
+++ services/tezos-platform-stats/bin/EndpointTest.hs
@@ -0,0 +1,24 @@
+module EndpointTest
+  ( main
+  ) where
+
+import TezosPlatformStats
+import Vest
+import qualified WebSocket
+
+webSocketConfig :: ResourceConfig WebSocket.T
+webSocketConfig =
+  WebSocket.Config
+    { WebSocket.servers =
+        [ ( Tagged $ namespace @TezosPlatformStats.T
+          , WebSocket.ServerInfo
+              {uri = Tagged "127.0.0.1", port = Tagged 3000, path = Tagged "/"})
+        ]
+    , WebSocket.servePort = Tagged 3000
+    , WebSocket.pingInterval = 30
+    }
+
+main :: IO ()
+main = do
+  with webSocketConfig $ \websocket -> testEndpoint websocket
+  return ()
--- services/tezos-platform-stats/src/TezosPlatformStats/Api.hs
+++ services/tezos-platform-stats/src/TezosPlatformStats/Api.hs
@@ -9,3 +9,16 @@ import qualified WebSocket
 
 type OriginationsByImplicitAddressEndpoint
    = EndpointJson 'NoAuth T WebSocket.T "originationsByImplicitAddress" Tezos.ImplicitAddress ('Direct [Tezos.OriginatedAddress])
+
+testEndpoint :: WebSocket.T -> IO [Tezos.OriginatedAddress]
+testEndpoint websocket = do
+  let binding =
+        makeClient
+          websocket
+          (Proxy :: Proxy OriginationsByImplicitAddressEndpoint)
+  res <- binding (Tagged $ Tagged "tz1Wit2PqodvPeuRRhdQXmkrtU8e8bRYZecd")
+  log
+    Debug
+    "originations for implicit address: tz1Wit2PqodvPeuRRhdQXmkrtU8e8bRYZecd"
+    res
+  return res
